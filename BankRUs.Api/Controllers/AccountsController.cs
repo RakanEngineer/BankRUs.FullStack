672eaa6bf2f529cfd5df3ba08a0f73062139dcfa
Robert Tublen <robert@educado.se>
2026-01-26 10:18:18 +0100

    Add ability to invoke OpenAccount use case

---
+++ BankRUs.Api/Controllers/AccountsController.cs
@@ -0,0 +1,36 @@
+﻿using BankRUs.Api.Dtos.Accounts;
+using BankRUs.Application.UseCases.OpenAccount;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace BankRUs.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController : ControllerBase
+{
+    private readonly OpenAccountHandler _openAccountHandler;
+
+    public AccountsController(OpenAccountHandler openAccountHandler)
+    {
+        _openAccountHandler = openAccountHandler;
+    }
+
+    // POST /api/accounts (Endpoint /  API endpoint)
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateAccountRequestDto requestDto)
+    {
+        var openAccountResult = await _openAccountHandler.HandleAsync(
+            new OpenAccountCommand(
+                FirstName: requestDto.FirstName,
+                LastName: requestDto.LastName,
+                SocialSecurityNumber: requestDto.SocialSecurityNumber,
+                Email: requestDto.Email));
+
+        var response = new CreateAccountResponseDto(openAccountResult.UserId);
+
+        // Returnera 201 Created
+        return Created(string.Empty, response);
+    }
+}
---
+++ BankRUs.Api/Dtos/Accounts/CreateAccountRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BankRUs.Api.Dtos.Accounts;
+
+// Använd record när: DTO, Command, Query
+// Använd class när: entitet
+public record CreateAccountRequestDto(
+    string FirstName,
+    string LastName,
+    string SocialSecurityNumber,
+    string Email
+);

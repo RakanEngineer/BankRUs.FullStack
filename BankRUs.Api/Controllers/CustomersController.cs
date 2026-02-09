341d3b2ca251fe10971daf274973375eb876cf20
RakanEngineer <rakan.engineer@gmail.com>
2026-02-09 16:17:14 +0100

    Explore attributes for OpenAPI and Scalar documentation

--- BankRUs.Api/Controllers/CustomersController.cs
+++ BankRUs.Api/Controllers/CustomersController.cs
@@ -3,7 +3,10 @@ using BankRUs.Application.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 using System.Security.Claims;
 using static BankRUs.Api.UseCases.Customers.DeleteCustomer;
 
@@ -46,19 +49,37 @@ namespace BankRUs.Api.Controllers
             return Ok(result);
         }
 
-        [Authorize(Roles = Roles.CustomerService)]
-        // GET /api/customers?ssn=19900101&page=1&pageSize=20
+        //[Authorize(Roles = Roles.CustomerService)]
+        //// GET /api/customers?ssn=19900101&page=1&pageSize=20
+        //[HttpGet]
+        //public async Task<IActionResult> GetCustomers(
+        //    [FromQuery] int page = 1,
+        //    [FromQuery] int pageSize = 20,
+        //    [FromQuery] string? ssn = null)
+        //{
+        //    var query = new GetCustomerQuery(page, pageSize, ssn);
+
+        //    var result = await _handler.Handle(query);
+
+        //    return Ok(result); 
+        //}
+
         [HttpGet]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(PagedResponseDto<CustomerItemDto>), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(ProblemDetails))]
+        [EndpointSummary("Get customers list")]
+        [EndpointDescription("Returns paginated list of customers")]
         public async Task<IActionResult> GetCustomers(
-            [FromQuery] int page = 1,
-            [FromQuery] int pageSize = 20,
+            [FromQuery][Description("Page number")] int page = 1,
+            [FromQuery][Description("Items per page")] int pageSize = 20,
             [FromQuery] string? ssn = null)
         {
             var query = new GetCustomerQuery(page, pageSize, ssn);
 
             var result = await _handler.Handle(query);
-
-            return Ok(result); 
+            return Ok(result);
         }
 
         [Authorize(Roles = Roles.Customer)]
